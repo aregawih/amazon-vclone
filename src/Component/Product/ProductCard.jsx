4a3f074fb6e727a8067b41646dac14c17d35ab46
aregawih <35622084+aregawih@users.noreply.github.com>
2024-09-22 21:38:07 +0300

    loading and detail page styling

--- src/Component/Product/ProductCard.jsx
+++ src/Component/Product/ProductCard.jsx
@@ -2,21 +2,22 @@ import React from 'react'
 import { Rating } from "@mui/material";
 import CurrencyFormat from '../CurrencyFormat/CurrencyFormat'
 import classes from "./Product.module.css"
-
-function ProductCard({product}) {
-  const {image, title, id, rating, price} = product;  
+import { Link } from "react-router-dom";
+function ProductCard({product,flex,renderDesc}) {
+  const {image, title, id, rating, price,description} = product;  
   return (
-    <div className={`${classes.card__container}`}>
-      <a href="">
-        <img src={image} alt="" />
-      </a>
+    <div className={`${classes.card__container} ${flex?classes.product__flexed:""}`}>
+      <Link to={`/products/${id}`}>
+        <img src={image} alt="" className={classes.img_container} />
+      </Link>
       <div>
         <h3>{title}</h3>
+        {renderDesc && <div style={{maxWidth:"750px"}}>{description}</div>}
         <div className={classes.rating}>
           {/* rating */}
-          <Rating value={rating.rate} precision={0.1} />
+          <Rating value={rating?.rate} precision={0.1} />
           {/* count  */}
-          <small>{rating.count}</small>
+          <small>{rating?.count}</small>
         </div>
         <div>
           {/* price  */}
--- src/Pages/Results/Results.jsx
+++ src/Pages/Results/Results.jsx
@@ -20,8 +20,6 @@ function Results() {
       
 }, [])
     
-
-
   return (
     <LayOut>
       <section>
