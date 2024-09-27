b4fd3ba971d18edad629eb44e0e8064ec773e7e8
aregawih <35622084+aregawih@users.noreply.github.com>
2024-09-27 15:04:05 +0300

    Building Payment page UI

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -7,17 +7,33 @@ import Orders from "./Pages/Orders/Orders";
 import Cart from "./Pages/Cart/Cart";
 import Results from "./Pages/Results/Results";
 import ProductDetail from "./Pages/ProductDetail/ProductDetail";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+const stripePromise = loadStripe(
+  "pk_test_51Q2hFiGS7Eq78DIwACESC5440os1hyFOSflJE4rGekaPetF4jHhsI5ZnjB3P9fUAkToZ8NPMM4IXTdcgypxruOk800MNa2jUJn"
+);
 
 function Routing() {
+
+
+
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/payments" element={<Payment />} />
+        <Route
+          path="/payments"
+          element={
+            <Elements stripe={stripePromise}>
+              <Payment />
+            </Elements>
+          }
+        />
         <Route path="/orders" element={<Orders />} />
         <Route path="/category/:categoryName" element={<Results />} />
-        <Route path="/products/:productId" element ={<ProductDetail/>} />
+        <Route path="/products/:productId" element={<ProductDetail />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
