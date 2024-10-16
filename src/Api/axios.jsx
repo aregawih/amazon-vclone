68a6fff784bc5f87f8c251c53425777a1ebe1e1b
aregawih <35622084+aregawih@users.noreply.github.com>
2024-10-16 13:36:16 +0300

    final amazon-clone

--- src/Api/axios.jsx
+++ src/Api/axios.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-a5498/us-central1/api",
+  baseURL: "http://localhost:5000/",
 });
 export {axiosInstance}
